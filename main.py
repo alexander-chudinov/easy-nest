c226134954f22927232c1f5f707a962edcb91f1e
Alexander Chudinov <alexander.chudinov@icloud.com>
2023-03-24 15:14:52 -0700

    added groupby layer

--- main.py
+++ main.py
@@ -2,47 +2,76 @@
 # open each file
 # remove Solidworks logo
 # save new file as .dxf
-
 import os
 import ezdxf
 
+# TODO: ask for thickness of balsa at start of program
+
+# Constants
 INPUT_FOLDER_PATH = './dxf_in/'
 OUTPUT_FOLDER_PATH = './dxf_out/'
 
-# if the directory does not exist, create it
-if not os.path.exists(INPUT_FOLDER_PATH):
-    os.makedirs(INPUT_FOLDER_PATH)
+# Initialize EasyNest file structure
+def createFileStructure():
+    # if the directory does not exist, create it
+    if not os.path.exists(INPUT_FOLDER_PATH):
+        os.makedirs(INPUT_FOLDER_PATH)
 
-if not os.path.exists(OUTPUT_FOLDER_PATH):
-    os.makedirs(OUTPUT_FOLDER_PATH)
+    if not os.path.exists(OUTPUT_FOLDER_PATH):
+        os.makedirs(OUTPUT_FOLDER_PATH)
 
-# scan in directory for all .dxf files
-for filename in os.listdir(INPUT_FOLDER_PATH):
-    if filename.lower().endswith(".dxf"):
-        print("Reading :", filename)
-        # open each file
-        doc = ezdxf.readfile(INPUT_FOLDER_PATH+filename)
+# Returns a list of files from input folder at INPUT_FOLDER_PATH
+def getAllInputDXFs():
+    filenames = []
+    for filename in os.listdir(INPUT_FOLDER_PATH):
+        if filename.lower().endswith(".dxf"):
+            filenames.append(filename)
+    return filenames
 
-        FILE_IN_MODEL_SPACE = False
-        FILE_IN_PAPER_SPACE = False
+# MODIFIES INPUT DXF doc model space
+# Finds and deletes SolidWorks logo entity from DXF model space
+def removeAttribution(msp, entities):
+    # get entities from layer
+    for entity in entities:
+        # remove Solidworks logo if found
+        if entity.dxftype() == 'MTEXT':
+            if entity.dxf.text == 'SOLIDWORKS Educational Product.' or entity.dxf.text == 'For Instructional Use Only.':
+                print("Found Solidworks logo")
+                msp.delete_entity(entity)
+                print("Deleted Solidworks logo")
 
-        # extract layers information
-        msp = doc.modelspace()
-        msp_grouped_layers = msp.groupby("layer")
-        model_layers = msp_grouped_layers.keys()
+# MODIFIES INPUT DXF doc model space
+def setLayerColor(entities, color):
+    for entity in entities:
+        entity.dxf.color = color
 
-        # get entities from each layer
-        for layer in model_layers:
-            print("Analyzing layer: ", layer)
-            entities = msp_grouped_layers.get(layer)
-            for entity in entities:
-                # remove Solidworks logo if found
-                if entity.dxftype() == 'MTEXT':
-                    if entity.dxf.text == 'SOLIDWORKS Educational Product.' or entity.dxf.text == 'For Instructional Use Only.':
-                        print("Found Solidworks logo")
-                        msp.delete_entity(entity)
-                        print("Deleted Solidworks logo")
+# Processes a single DXF file
+def processFile(filename):
+     # open file
+    doc = ezdxf.readfile(INPUT_FOLDER_PATH+filename)
 
-        # # save new file as .dxf
-        print("Writing :", filename)
-        doc.saveas(OUTPUT_FOLDER_PATH+filename, encoding='utf-8')
+    # extract layers information
+    msp = doc.modelspace()
+    msp_grouped_layers = msp.groupby("layer")
+    model_layers = msp_grouped_layers.keys()
+
+    for layer in model_layers:
+        entities = msp_grouped_layers.get(layer)
+        removeAttribution(msp, entities)
+        setLayerColor(entities, 256)
+
+    # save file
+    print("Processed file: " + filename)
+    doc.saveas(OUTPUT_FOLDER_PATH+filename, encoding='utf-8')
+
+
+# MAIN
+if __name__ == "__main__":
+    # Initialize EasyNest file structure
+    createFileStructure()
+
+    # scan in directory for all .dxf files
+    filenames = getAllInputDXFs()
+
+    for filename in filenames:
+        processFile(filename)
